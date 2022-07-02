eb1322a1b232b888de20bbb7b1dfcdbc2002d114
César Cordera <ccordera@opentrends.net>
2022-07-02 13:16:19 +0200

    ultima leccion

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -8,10 +9,54 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class NoComunesComponent implements OnInit {
 
+  nombre:string = 'Susana';
+  genero: string = 'femenino';
+
+  invitacionMap = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+
+  clientes: string[] = ['Maria', 'Pedro', 'Juan'];
+  clientesMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'Tenemos # clientes esperando'
+  }
+
+  
+  // Keyvalue Pipe
+  persona = {
+    nombre: 'Fernando',
+    edad: 35,
+    direccion: 'Ottawa, Canada'
+  }
+
   constructor(private primengConfig: PrimeNGConfig) { }
+
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
   }
+
+  cambiarCliente() {
+    this.nombre = this.nombre==='Susana' ? 'Fernando' : 'Susana';
+    if(this.nombre === 'Susana') {
+      this.genero = 'femenino';
+    } else {
+      this.genero = 'masculino';
+    }
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
+  }
+
+  //Async Pipe
+  miObservable = interval(1000);
+  myPromise = new Promise( 
+    (resolve, reject) => setTimeout( () => resolve('YATA!!'), 5000 )
+  )
+
 
 }
